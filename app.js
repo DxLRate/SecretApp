287f4b4ee5a6d0c6bd3280709fb743731138b4ea
Sourabh Prasad <saurabh3052000@gmail.com>
2022-08-05 13:01:13 +0530

    deployment phase 2

--- app.js
+++ app.js
@@ -200,10 +200,14 @@ app.post("/login", function(req, res){
 
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
 
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000.");
 });
